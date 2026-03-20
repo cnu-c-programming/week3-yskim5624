1d1bfb85bd3b8b3104eac15f583905c8b6ba8d5b
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-20 09:26:27 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void print();
+int square(int n);
+
+int main()
+{
+    print();
+
+    int num = 5;
+    int result = square(num);
+    printf("%d %d\n", num, result);
+
+    return 0;
+}
+
+void print() {
+    printf("Welcome to Computer Programming 03 Lab!\n");
+}
+
+int square(int n) {
+    return n * n;
+}
