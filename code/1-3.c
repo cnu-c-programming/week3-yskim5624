1f6f0cbd65bb260de404fe02a69c10bc8234fb5e
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-20 09:34:40 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int power(int base, int exp) {
+    if(exp == 0) return 1;
+    else return base * power(base, exp - 1);
+}
+
+int main()
+{
+    printf("%d\n", power(2, 3));
+    printf("%d\n", power(5, 0));
+    printf("%d\n", power(3, 4));
+
+    return 0;
+}
