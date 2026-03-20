6344d476afc0a1df3381dbe58113a54408645c0b
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-20 09:14:59 +0900

    Update 3-1.c

---
+++ code/3-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int call_count() {
+    static int count = 0;
+    return ++count;
+}
+
+int main()
+{
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    
+    return 0;
+}
